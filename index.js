fc47d91cf17a30461082446587b180adb38c179f
tackman <tackman@tackman.info>
2018-12-13 22:41:51 +0900

    tf prediction just ran

---
+++ index.js
@@ -0,0 +1,14 @@
+async function run(x) {
+  const model = await tf.loadModel('nets/ep100.tfjs/model.json');
+
+  let pred = model.predict(x);
+  //  pred.print();
+  console.log(pred.shape);
+
+  let transposed = tf.transpose(pred);
+  console.log(transposed.shape);
+  const d = await pred.data();
+  return d;
+}
+
+// run();
