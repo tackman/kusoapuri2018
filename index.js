1928151a599550b0f5cfc3fd0d831bf1f31f5938
tackman <tackman@tackman.info>
2018-12-14 21:04:02 +0900

    removed debug log

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 async function run(x, modelPath) {
-
   const model = await tf.loadModel(modelPath);
 
   let pred = model.predict(x);
-  //  pred.print();
-  console.log(pred.shape);
 
   let transposed = tf.transpose(pred);
-  console.log(transposed.shape);
   const d = await transposed.data();
   return d;
 }
